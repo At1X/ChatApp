f3ff0f68865fce41092730d7670714743b62b213
At1X <a.khodaei@ec.iut.ac.ir>
2025-03-31 19:38:59 +0330

    fix socket url

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -4,7 +4,7 @@ let socket;
 
 // connecting to socket io server from backend
 const connectSocket = (token) => {
-  socket = io(process.env.REACT_APP_API_ORIGIN.split("/api")[0], {
+  socket = io(process.env.REACT_APP_SOCKET_ORIGIN, {
     query: `token=${token}`,
   });
 };
